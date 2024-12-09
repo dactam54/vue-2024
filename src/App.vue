604b9351f7826d4f9b3b68c8d60d99c966587d0d
dactam54 <tamphi67@gmail.com>
2024-12-09 21:09:55 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from "vue";
+
+const name = ref("Hello Vue 3");
+const status = ref(true);
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status">User is active</p>
+</template>
